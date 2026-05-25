b8e6b97a5bcbb5a1354ee485d0a9416da11047c9
digitalxprespro-gif <digitalxprespro@gmail.com>
2026-05-26 05:21:55 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, send_from_directory
+
 app = Flask(__name__, static_folder='.')
 
 @app.route('/')
 def index():
     return send_from_directory('.', 'index.html')
+
+@app.route('/download_video')
+def download_video():
+    # এখানে আপনার এডিট করা ভিডিও ফাইলটি থাকতে হবে
+    return send_from_directory('.', 'final_video.mp4')
 
 if __name__ == '__main__':
     app.run()
